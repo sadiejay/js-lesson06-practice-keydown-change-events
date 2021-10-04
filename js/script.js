0c8b3305380943156d9a452a312a89a4b5255db1
sadiejay <19538219+sadiejay@users.noreply.github.com>
2021-10-04 00:13:57 -0500

    Adds event listener to dropdown menu

--- js/script.js
+++ js/script.js
@@ -12,3 +12,20 @@ document.addEventListener("keydown", function (e) {
 
  }
 });
+
+
+
+var fave = document.querySelector("#favorite");
+var heading = document.querySelector("h1");
+var selection = "regular";
+ 
+fave.addEventListener("change", function (e) {
+ selection = e.target.value;
+ if (selection === "stealth") {
+   heading.innerText = "Stealth Quincy 😎";
+ } else if (selection === "party") {
+   heading.innerText = "Party Quincy 🥳";
+ } else {
+   heading.innerText = "Quincy";
+ }
+});
